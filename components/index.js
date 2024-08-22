9c4fbbe64b4fe52e270dac23df8df3124656907a
demon <naimadokwas@gmail.com>
2024-08-22 14:52:49 +0200

    more components

--- components/index.js
+++ components/index.js
@@ -1,21 +1,21 @@
-// import VideoCard from "./VideoCard"
+import VideoCard from "./VideoCard"
 import FormField from "./FormField"
 import CustomButton from "./CustomButton"
-// import InfoBox from "./InfoBox"
-// import Loader from "./Loader"
-// import Trending from "./Trending"+
-// import SearchInput from "./SearchInput"
-// import EmptyState from "./EmptyState"
+import InfoBox from "./InfoBox"
+import Loader from "./Loader"
+import Trending from "./Trending"+
+import SearchInput from "./SearchInput"
+import EmptyState from "./EmptyState"
 
 export {
-  // VideoCard,
+  VideoCard,
   FormField,
   CustomButton,
-  // InfoBox,
-  // Loader,
-  // Trending,
-  // SearchInput,
-  // EmptyState,
+  InfoBox,
+  Loader,
+  Trending,
+  SearchInput,
+  EmptyState,
 }
 
 // umowa zlecenie, umowa o prace na czas nieokreślony - od 4 do 6 miesięcy.
